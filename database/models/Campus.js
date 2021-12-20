c211e6ab3f77438c60ba3b3d84da5a16db43c248
katie34 <89612931+katie34@users.noreply.github.com>
2021-12-20 15:55:32 -0500

    adding address and image parameters to campus

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -10,7 +10,20 @@ const Campus = db.define("campus", {
 
   description: {
     type: Sequelize.STRING,
-  }
+  },
+
+  address: {
+    type: Sequelize.STRING,
+  },
+
+  imgurl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      "https://images.unsplash.com/photo-1576495199011-eb94736d05d6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2FtcHVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    validate: {
+      isUrl: true,
+    },
+  },
 
 });
 
--- database/models/Student.js
+++ database/models/Student.js
@@ -11,7 +11,7 @@ const Student = db.define("student", {
   lastname: {
     type: Sequelize.STRING,
     allowNull: false
-  }
+  },
   
   email: {
     type: Sequelize.STRING,
