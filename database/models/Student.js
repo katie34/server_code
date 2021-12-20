fc70a89f3786b9672bd1db2562be3c5b8962f433
katie34 <89612931+katie34@users.noreply.github.com>
2021-12-20 15:53:43 -0500

    dding email, image and gpa parameters to student

--- database/models/Student.js
+++ database/models/Student.js
@@ -12,6 +12,27 @@ const Student = db.define("student", {
     type: Sequelize.STRING,
     allowNull: false
   }
+  
+  email: {
+    type: Sequelize.STRING,
+  //   allowNull: false
+  },
+
+  imageurl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      "https://media.istockphoto.com/photos/facing-my-future-with-confidence-picture-id1139495117?k=6&m=1139495117&s=612x612&w=0&h=a2hxTvHq7XNN0zMh8m1CuyDPIaNYiswD7iZbh5rpac4=",
+    validate: {
+      isUrl: true,
+    },
+  },
+  
+gpa: {
+    type: Sequelize.STRING,
+    defaultValue: "N/A",
+    isDecimal: true,
+    validate: {min: 0.0, max: 4.0 },
+  },
 
 });
 
